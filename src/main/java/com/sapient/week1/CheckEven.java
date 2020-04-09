d2a82dc0f6bb2cf9bc7e1cd5cd0bf274ea86b4a8
PavanPublicis <PavanPublicis@gmail.com>
2020-04-09 18:51:30 +0530

    Assignment done

---
+++ src/main/java/com/sapient/week1/CheckEven.java
@@ -0,0 +1,14 @@
+package com.sapient.week1;
+
+
+public class CheckEven 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static boolean check(int n)
+    {
+	return (n % 2 == 0);
+    }
+}
